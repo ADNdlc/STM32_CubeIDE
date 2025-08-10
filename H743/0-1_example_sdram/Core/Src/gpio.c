82784d4497834497dc3dc90b0ab812fdef5b702e
ADMddlc <1211428948@qq.com>
2025-08-10 22:05:03 +0800

    移除

--- H743/0-1_example_sdram/Core/Src/gpio.c
+++ H743/0-1_example_sdram/Core/Src/gpio.c
@@ -7,7 +7,7 @@
   ******************************************************************************
   * @attention
   *
-  * Copyright (c) 2025 STMicroelectronics.
+  * Copyright (c) 2025 STMicroelectronics. 
   * All rights reserved.
   *
   * This software is licensed under terms that can be found in the LICENSE file
@@ -29,6 +29,7 @@
 /* Configure GPIO                                                             */
 /*----------------------------------------------------------------------------*/
 /* USER CODE BEGIN 1 */
+
 
 /* USER CODE END 1 */
 
